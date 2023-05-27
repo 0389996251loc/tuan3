3a1e4f542e92e3f397c0355bfef499fa26e4fc9e
tvloc <tvlocabc12@gmail.com>
2023-05-27 16:08:55 +0700

    tuan3

--- src/main/java/com/example/bookstore/controller/BookController.java
+++ src/main/java/com/example/bookstore/controller/BookController.java
@@ -2,18 +2,26 @@ package com.example.bookstore.controller;
 
 import com.example.bookstore.entity.Book;
 import com.example.bookstore.services. BookService;
+import com.example.bookstore.services.CategoryService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation. Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 @Controller
 @RequestMapping("/books")
+
 public class BookController {
     @Autowired
     private BookService bookService;
+    @Autowired
+    private CategoryService categoryService;
 
     @GetMapping
     public String showAllBooks(Model model) {
@@ -21,4 +29,17 @@ public class BookController {
         model.addAttribute("books", books);
         return "book/list";
     }
-}
+    @GetMapping("/add")
+    public String addBookFor(Model model){
+        model.addAttribute("book", new Book());
+        model.addAttribute("categories", categoryService.getAllCategories());
+        return "book/add";
+    }
+    @PostMapping("/add")
+    public String addBook(@ModelAttribute("Book")Book book){
+        bookService.addBook(book);
+        return "redirect:/books";
+        }
+    }
+
+
--- src/main/java/com/example/bookstore/entity/Book.java
+++ src/main/java/com/example/bookstore/entity/Book.java
@@ -1,7 +1,13 @@
 package com.example.bookstore.entity;
 
+import com.example.bookstore.Validator.annotation.ValidCategoryId;
+import com.example.bookstore.Validator.annotation.ValidCategoryId;
+import com.example.bookstore.Validator.annotation.ValidUserId;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import  lombok.Data;
+import lombok.NonNull;
+
 @Data
 @Entity
 @Table(name= "book")
@@ -9,13 +15,26 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(name = "title")
+    @NotEmpty(message = "Title mus be empty")
+    @Size(max = 50 ,min=1, message ="Title must be less than 50 characters")
     private String title;
+
     @Column(name = "author")
     private String author;
+
     @Column(name = "price")
+    @NotNull(message = "Price is required")
     private Double price;
+
     @ManyToOne
     @JoinColumn(name = "category_id")
+    @ValidCategoryId
     private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @ValidUserId
+    private User user;
 }
